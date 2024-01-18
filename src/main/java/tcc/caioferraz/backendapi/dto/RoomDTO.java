bcfb8dc61bce414cd12dc0ed3b03dcff8670fed7
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-18 17:50:10 -0300

    feat(dto): add validation constraints to RoomDTO

--- src/main/java/tcc/caioferraz/backendapi/dto/RoomDTO.java
+++ src/main/java/tcc/caioferraz/backendapi/dto/RoomDTO.java
@@ -1,12 +1,22 @@
 package tcc.caioferraz.backendapi.dto;
 
-import tcc.caioferraz.backendapi.domain.rooms.RoomEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
 
+@Getter
 public record RoomDTO(
-        Integer number,
-        RoomEnum type,
+        @NotBlank(message = "O número do quarto não pode estar em branco ou nulo!")
+        String number,
+        @NotBlank(message = "O tipo do quarto não pode estar em branco ou nulo!")
+        String type,
+        @NotBlank(message = "A quantidade de camas do quarto não pode estar em branco ou nulo!")
+        @Positive(message = "A quantidade de camas deve ser superior a um(1)!")
         Integer bedQuantity,
+        @NotBlank(message = "A descrição do quarto não pode estar em branco ou nulo!")
         String description,
+        @NotBlank(message = "O valor de diária do quarto não pode estar em branco ou nulo!")
+        @Positive(message = "O valor de diária do quarto deve ser um numero positivo!")
         Double dailyRate
 ) {
 }
