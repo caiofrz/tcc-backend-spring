dc9026843dcb03661f8d574c1aa69a469af825b4
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-19 17:18:44 -0300

    fix(rooms): change field dailyRate type from Double to BigDecimal, and remove validations NotBlank to fields dailyRate n bedQuantity

--- src/main/java/tcc/caioferraz/backendapi/dto/RoomDTO.java
+++ src/main/java/tcc/caioferraz/backendapi/dto/RoomDTO.java
@@ -3,18 +3,18 @@ package tcc.caioferraz.backendapi.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
+import java.math.BigDecimal;
+
 public record RoomDTO(
         @NotBlank(message = "O número do quarto não pode estar em branco ou nulo!")
         String number,
         @NotBlank(message = "O tipo do quarto não pode estar em branco ou nulo!")
         String type,
-        @NotBlank(message = "A quantidade de camas do quarto não pode estar em branco ou nulo!")
         @Positive(message = "A quantidade de camas deve ser superior a um(1)!")
         Integer bedQuantity,
         @NotBlank(message = "A descrição do quarto não pode estar em branco ou nulo!")
         String description,
-        @NotBlank(message = "O valor de diária do quarto não pode estar em branco ou nulo!")
         @Positive(message = "O valor de diária do quarto deve ser um numero positivo!")
-        Double dailyRate
+        BigDecimal dailyRate
 ) {
 }
